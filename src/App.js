c7917901f94e58af9981f606815cc5140949f6e4
Evelynn Gresbach <egresbach1@my.wctc.edu>
2022-01-24 13:47:55 -0600

    Random countries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Container } from '@mui/material';
+import { Container, Button } from '@mui/material';
 import './App.css';
 import Country from './components/Country';
 
@@ -9,7 +9,31 @@ class App extends Component{
       { id: 1, name: 'United States', goldMedalCount: 2 },
       { id: 2, name: 'China', goldMedalCount: 3 },
       { id: 3, name: 'Germany', goldMedalCount: 0 },
+    ],
+    randomCountryNames: [
+      "Iceland", "Tuvalu", "Israel", "Estonia", "Saint Lucia", "Mayotte"
     ]
+  }
+
+  handleCountryDelete = (id) => {
+    this.setState({countries: this.state.countries.filter(c => c.id !== id)})
+  }
+  addCountry = () => {
+    console.log(this.state.randomCountryNames.length)
+    if(this.state.randomCountryNames.length > 0){
+      //generate random for name and gold medal count
+      let rnd = Math.floor(Math.random() * this.state.randomCountryNames.length);
+      let name = this.state.randomCountryNames[rnd];
+      let id = Math.max.apply(Math, this.state.countries.map(c => c.id)) + 1; 
+
+      //remove name to prevent duplicates
+      this.setState({randomCountryNames: this.state.randomCountryNames.filter(n => n !== name)})
+
+      //add to countries
+      var countries = this.state.countries; 
+      countries.push({id: id, name: name, goldMedalCount: rnd });
+      this.setState({countries: countries}); 
+    }
   }
 
   render(){
@@ -19,8 +43,10 @@ class App extends Component{
           {this.state.countries.map(country =>
               <Country key={country.id}
                 country = {country}
+                onDelete = {this.handleCountryDelete}
               />
             )}
+            <Button variant="contained" onClick={this.addCountry} disabled={this.state.randomCountryNames.length === 0}>Add Random Country</Button>
         </Container>
       </div>
     )
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Avatar, Card, CardContent, Divider, IconButton } from "@mui/material"
+import { Avatar, Button, Card, CardContent, CardActions, Divider, IconButton } from "@mui/material"
 import { withStyles } from "@mui/styles";
 import { AddCircle, RemoveCircle } from "@mui/icons-material"
 
@@ -23,6 +23,7 @@ class Country extends Component {
     state = {
         name: this.props.country.name, 
         gold: this.props.country.goldMedalCount,
+        id: this.props.country.id
     }
 
     handleIncrement = () => {
@@ -35,20 +36,20 @@ class Country extends Component {
     render() {
         const { classes } = this.props;
         return(
-            <Card className={classes.card}>
-                <div className="country">
-                    <CardContent>
-                        <div className={classes.countryName}>{this.state.name}</div>
-                        <Divider></Divider>
-                        <div> 
-                            <Avatar className={classes.medals} >{this.state.gold}</Avatar>
-                            <span style={{fontSize: "1.5em"}}> Gold Medal{this.state.gold === 1 ? '' : 's'} </span> 
-                        </div> 
-                        <IconButton size="small" className="increment" onClick={this.handleIncrement}><AddCircle /></IconButton>
-                        <IconButton size="small" className="decrement" onClick={this.handleDecrement} disabled={this.state.gold === 0}><RemoveCircle /></IconButton>
-                    </CardContent>
-                    
-                </div>
+            <Card className={classes.card}>            
+                <CardContent>
+                    <div className={classes.countryName}>{this.state.name}</div>
+                    <Divider></Divider>
+                    <div> 
+                        <Avatar className={classes.medals} >{this.state.gold}</Avatar>
+                        <span style={{fontSize: "1.5em"}}> Gold Medal{this.state.gold === 1 ? '' : 's'} </span> 
+                    </div> 
+                    <IconButton size="small" className="increment" onClick={this.handleIncrement}><AddCircle /></IconButton>
+                    <IconButton size="small" className="decrement" onClick={this.handleDecrement} disabled={this.state.gold === 0}><RemoveCircle /></IconButton>
+                </CardContent>
+                <CardActions>
+                    <Button variant="outlined" onClick={() => this.props.onDelete(this.state.id)}>Remove Country</Button>
+                </CardActions>
             </Card>
             
         );
